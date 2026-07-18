1b2b1bcd3cc71bf04a21915297688e34a58f48b0
android-tms <jewel@ticonsys.com>
2026-07-18 10:13:39 +0600

    Update MODEL_4B download link in Constants
    - Update `MODEL_4B` URL in `Constants.kt` to use a Google Drive direct download link.

--- app/src/main/java/com/dutchman/resumeiq/domain/util/Constants.kt
+++ app/src/main/java/com/dutchman/resumeiq/domain/util/Constants.kt
@@ -3,5 +3,6 @@ package com.dutchman.resumeiq.domain.util
 object Constants {
 
     const val MODEL_2B = "https://drive.google.com/file/d/13uLTvCY67CZ8UzMSyJmMq7YXZFqOKTJw/view?usp=drive_link"
-    const val MODEL_4B = "https://huggingface.co/gummybear2555/Gemma-3n-E2B-it-int4/resolve/main/gemma-3n-E2B-it-int4.task?download=true"
+//    const val MODEL_4B = "https://huggingface.co/gummybear2555/Gemma-3n-E2B-it-int4/resolve/main/gemma-3n-E2B-it-int4.task?download=true"
+    const val MODEL_4B = "https://drive.usercontent.google.com/download?id=13uLTvCY67CZ8UzMSyJmMq7YXZFqOKTJw&export=download&confirm=t&uuid=0678aed5-a89d-4a9b-8ca1-322d1d3d001c"
 }
